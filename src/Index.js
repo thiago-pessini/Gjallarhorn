f4aa0cf42ce2d97abddb257ddccfb3eac526c1ff
Thiago Pessini <thiago.pessini@outlook.com>
2018-12-02 19:43:48 -0200

    Added feature to load file created date as the test date

--- src/Index.js
+++ src/Index.js
@@ -1,27 +1,33 @@
 const request = require('request');
 
 console.log('Loading parameters...');
+let talkei = true; //This variable define if it is everything ok to the normal execution
 const kvasir_api_version = process.env.KVASIR_API_VERSION || 1;
 const params = {
     report_file: process.env.REPORT_FILE || "cucumber_report.json",
-    kvasir_address: process.env.KVASIR_HOST || `http://localhost:4000/api/v${kvasir_api_version}/test`,
+    kvasir_address: process.env.KVASIR_HOST || `http://localhost:3000/api/v${kvasir_api_version}/test`,
     project_name: process.env.PROJECT_NAME,
-    enviroment: process.env.ENVIRONMENT
+    enviroment: process.env.ENVIRONMENT,
+    use_current_date: (typeof process.env.USE_CURRENT_DATE !== 'undefined')
 };
 
 if (!params.project_name) {
     console.error("Project name was not defined. You should define PROJECT_NAME variable before start the application.\nFor example 'export PROJECT_NAME=Analytics'");
+    talkei = false;
     process.exitCode = 1;
 };
 
 if (!params.enviroment) {
     console.error("Environment was not defined. You should define ENVIRONMENT variable before start the application.\nFor example 'export ENVIRONMENT=WEB'");
+    talkei = false;
     process.exitCode = 1;
 };
-if (params.project_name && params.enviroment) {
+
+if (talkei) {
     console.log('Parameters were loaded successfully.');
     let fs = require("fs");
     let report = JSON.parse(fs.readFileSync(params.report_file));
+    const fileDate = params.use_current_date ? Date.now() : fs.statSync(params.report_file).birthtime;
     let scenarios = [];
     for (let i = 0; i < report.length; i++) {
         let tests = [];
@@ -32,7 +38,7 @@ if (params.project_name && params.enviroment) {
              * Its necessary investigate if that step is part of all steps or
              * if its a default step for every test case (scenario in cucumber scope)
              */
-            for (let k = 0; k < report[i].elements[j].steps.length-1; k++) {
+            for (let k = 0; k < report[i].elements[j].steps.length - 1; k++) {
                 steps[k] = {
                     description: report[i].elements[j].steps[k].name,
                     status: report[i].elements[j].steps[k].result.status,
@@ -49,17 +55,20 @@ if (params.project_name && params.enviroment) {
             project: params.project_name,
             environment: params.enviroment,
             description: report[i].description.trim().replace(new RegExp('\n    ', 'gi'), ', '),
-            executed_at: Date.now(),
+            executed_at: fileDate,
             tests: tests
         };
     };
 
-    // console.log(JSON.stringify(scenarios));
     request.post(params.kvasir_address, {
         json: scenarios
     },
         (error, response, body) => {
-            console.log(response.statusCode);
+            if(response && response.statusCode === 201){
+                console.info(`Request made successfully to ${params.kvasir_address}`);
+            }else{
+                console.error(`An error occurred during the request\nError: ${error}\nResponse:`, body);
+            }
         }
     );
 };
